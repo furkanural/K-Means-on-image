fd7e79444345598c48316fb131060e28497c2e25
Furkan URAL <uralfrkn@gmail.com>
2016-11-24 13:02:14 +0300

    Initial Commit

---
+++ homework_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  homework_2
+//
+//  Created by Furkan Ural on 24/11/2016.
+//  Copyright © 2016 Furkan Ural. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
